0182a964c134610f211ddcd9755968ebd8b5d577
Daniel Swinney <daniel.swinney@gmail.com>
2026-01-13 15:43:31 -0600

    Adds most played titles to user profile

--- engine/models/user.py
+++ engine/models/user.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from engine.models.game import GamePlayer
+from engine.models.game import GamePlayer, Title
 
 class UserProfile(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -17,6 +17,22 @@ class UserProfile(models.Model):
         gps = GamePlayer.objects.filter(user=self, game__ended_at__isnull=True, is_active=True)
         games = [gp.game for gp in gps]
         return games
+
+    @property
+    def most_played_titles(self) -> dict:
+        gps = GamePlayer.objects.filter(user=self)
+        title_count = {}
+        from engine.serializers import TitleSerializer
+
+        for gp in gps:
+            title = TitleSerializer(gp.game.title).data
+            title_id = gp.game.title.id
+            if title_id in title_count:
+                title_count[title_id] += 1
+            else:
+                title_count[title_id] = 1
+        sorted_titles = sorted(title_count.items(), key=lambda x: x[1], reverse=True)
+        return {str(title_id): { 'count': count, 'title': TitleSerializer(Title.objects.get(id=title_id)).data } for title_id, count in sorted_titles}
 
     def __str__(self):
         return self.display_name or self.user.username
--- engine/serializers.py
+++ engine/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 
-from engine.models.game import Game
-from .models.user import UserProfile
-
 class GameSerializer(serializers.ModelSerializer):
     class Meta:
+        from engine.models.game import Game
         model = Game
+        fields = '__all__'
+
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        from engine.models.game import Title
+        model = Title
         fields = '__all__'
 
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
+        from .models.user import UserProfile
         model = UserProfile
         fields = ['id', 'user', 'display_name', 'profile_image', 'public_listing', 'over_18']
         read_only_fields = ['user']
@@ -19,8 +24,10 @@ class UserProfileSerializerFull(serializers.ModelSerializer):
     first_name = serializers.CharField(source='user.first_name', read_only=True)
     last_name = serializers.CharField(source='user.last_name', read_only=True)
     games = GameSerializer(many=True, read_only=True, source='ongoing_games')
+    most_played_titles = serializers.DictField(read_only=True)
 
     class Meta:
+        from .models.user import UserProfile
         model = UserProfile
-        fields = ['id', 'user', 'display_name', 'profile_image', 'public_listing', 'over_18', 'email', 'first_name', 'last_name', 'games']
+        fields = ['id', 'user', 'display_name', 'profile_image', 'public_listing', 'over_18', 'email', 'first_name', 'last_name', 'games', 'most_played_titles']
         read_only_fields = ['user']
